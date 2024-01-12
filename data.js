c4211dedcb531198cb6bcb49d76b34f5a9ba9b90
erso <ersin99mehmed@abv.bg>
2024-01-12 21:48:50 +0200

    fix problem with pie chart period selecting

--- data.js
+++ data.js
@@ -12,34 +12,44 @@ const periods = [
   { value: "Последната година" },
 ];
 
+const dropdownPeriods = [
+  "Днес",
+  "Вчера",
+  "Последните 7 дни",
+  "Последният месец",
+  "Последните 3 месеца",
+  "Последните 6 месеца",
+  "Последната година",
+];
+
 const categories = ["Бутилки", "Балони", "Накрайници"];
 
 const dashboardLinks = [
   {
     link: "/dashboard",
     text: "Табло",
-    icon: <BsHouse className="w-5 h-5" />,
+    icon: <BsHouse className='w-5 h-5' />,
   },
   {
     link: "/dashboard/orders",
     text: "Поръчки",
-    icon: <BsTruck className="w-5 h-5" />,
+    icon: <BsTruck className='w-5 h-5' />,
   },
   {
     link: "/dashboard/stocks",
     text: "Наличности",
-    icon: <BsBox className="w-5 h-5" />,
+    icon: <BsBox className='w-5 h-5' />,
   },
   {
     link: "/dashboard/sales",
     text: "Продажби",
-    icon: <BsCart2 className="w-5 h-5" />,
+    icon: <BsCart2 className='w-5 h-5' />,
   },
   {
     link: "/dashboard/ads",
     text: "Реклами",
-    icon: <BsMegaphone className="w-5 h-5" />,
+    icon: <BsMegaphone className='w-5 h-5' />,
   },
 ];
 
-export { periods, perPageResult, dashboardLinks, categories };
+export { periods, dropdownPeriods, perPageResult, dashboardLinks, categories };
