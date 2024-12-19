2efe9b0a3b8036ce9ecc97489edb1333036440ae
erso <ersin99mehmed@abv.bg>
2024-12-19 15:34:12 +0200

    Update next.js version

--- components/table/Pagination.js
+++ components/table/Pagination.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useMemo } from "react";
+import React, { useCallback } from "react";
 import Button from "./PaginationButton";
 
 function Pagination(props) {
@@ -12,14 +12,11 @@ function Pagination(props) {
     startPage = Math.max(1, props.totalPages - 4);
   }
 
-  const handlePageClick = useMemo(
-    () => (page) => {
-      if (props.currentPage !== page) {
-        props.handlePageClick(page);
-      }
-    },
-    [props.currentPage, props.handlePageClick]
-  );
+  const handlePageClick = (page) => {
+    if (props.currentPage !== page) {
+      props.handlePageClick(page);
+    }
+  };
 
   const pageButtons = [];
 
@@ -38,10 +35,9 @@ function Pagination(props) {
     return (
       <div
         key={key}
-        className="rounded-lg w-9 h-9 leading-tight bg-[#f4f4f5] px-2"
-      >
-        <div className="h-1 bg-gray-200 rounded-full w-10/12 mt-3 mb-1"></div>
-        <div className="h-1 bg-gray-200 rounded-full w-full"></div>
+        className='rounded-lg w-9 h-9 leading-tight bg-[#f4f4f5] px-2'>
+        <div className='h-1 bg-gray-200 rounded-full w-10/12 mt-3 mb-1'></div>
+        <div className='h-1 bg-gray-200 rounded-full w-full'></div>
       </div>
     );
   };
@@ -57,21 +53,21 @@ function Pagination(props) {
   };
 
   return (
-    <nav className="flex items-center space-x-1.5 h-8 text-sm">
+    <nav className='flex items-center space-x-1.5 h-8 text-sm'>
       {props.isLoading ? (
         renderSkeleton(7)
       ) : (
         <>
           {props.totalPages > 5 && props.currentPage > 3 && (
-            <div className="flex items-end space-x-2">
+            <div className='flex items-end space-x-2'>
               <Button
-                text="1"
+                text='1'
                 check={props.currentPage === 1}
                 onClick={() => handlePageClick(1)}
               />
 
               <Button
-                dotsButton="left"
+                dotsButton='left'
                 check={props.currentPage === 1}
                 onClick={() => handlePageClick(1)}
               />
@@ -82,9 +78,9 @@ function Pagination(props) {
 
           {props.totalPages > 5 &&
             props.currentPage <= props.totalPages - 3 && (
-              <div className="flex items-end space-x-2">
+              <div className='flex items-end space-x-2'>
                 <Button
-                  dotsButton="right"
+                  dotsButton='right'
                   check={props.currentPage === props.totalPages}
                   onClick={() => handlePageClick(props.totalPages)}
                 />
