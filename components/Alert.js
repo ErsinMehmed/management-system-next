465ca8d5200125e2e54c35692ed0d3fe08916855
ersin <ersin.hyusein@mypos.com>
2025-07-30 14:15:52 +0300

    added hero ui toast component

--- components/Alert.js
+++ components/Alert.js
@@ -1,44 +1,34 @@
-import React, { useEffect, useCallback } from "react";
-import { motion } from "framer-motion";
-import { commonStore } from "@/stores/useStore";
+import React, {useEffect} from "react";
+import {commonStore} from "@/stores/useStore";
+import {addToast} from "@heroui/react";
 
 const Alert = () => {
-  const { setErrorMessage, setSuccessMessage, errorMessage, successMessage } =
-    commonStore;
+    const {setErrorMessage, setSuccessMessage, errorMessage, successMessage} =
+        commonStore;
 
-  const handleHideAlert = useCallback(() => {
-    setErrorMessage("");
-    setSuccessMessage("");
-  }, [setErrorMessage, setSuccessMessage]);
+    useEffect(() => {
+        if (errorMessage) {
+            addToast({
+                description: errorMessage,
+                color: "red",
+                hideIcon: false,
+            });
 
-  useEffect(() => {
-    if (errorMessage || successMessage) {
-      const timeoutId = setTimeout(() => {
-        handleHideAlert();
-      }, 3000);
+            setErrorMessage("");
+        }
 
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [errorMessage, successMessage, handleHideAlert]);
+        if (successMessage) {
+            addToast({
+                description: successMessage,
+                color: "success",
+                hideIcon: false,
+            });
 
-  return (
-    (successMessage || errorMessage) && (
-      <motion.div
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.7 }}
-        className={`fixed top-12 sm:top-5 -ml-[165px] sm:-ml-[180px] left-1/2 center-element text-sm sm:text-base w-80 sm:w-[450px] py-3.5 sm:py-4 mb-4 z-[1000] ${
-          errorMessage
-            ? "text-red-600 bg-red-100"
-            : "text-green-600 bg-green-100"
-        }  rounded-lg font-medium`}
-      >
-        {errorMessage || successMessage}
-      </motion.div>
-    )
-  );
+            setSuccessMessage("");
+        }
+    }, [errorMessage, successMessage, setErrorMessage, setSuccessMessage]);
+
+    return null;
 };
 
 export default Alert;
--- providers/MainProvider.js
+++ providers/MainProvider.js
@@ -1,6 +1,15 @@
 "use client";
-import { HeroUIProvider } from "@heroui/react";
+import {HeroUIProvider} from "@heroui/react";
+import {ToastProvider} from "@heroui/toast";
+import {useRouter} from 'next/navigation'
 
-export default function Providers({ children }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+export default function Providers({children}) {
+    const router = useRouter()
+
+    return (
+        <HeroUIProvider navigate={router.push}>
+            <ToastProvider placement="top-center" toastOffset={25}/>
+            {children}
+        </HeroUIProvider>
+    )
 }
