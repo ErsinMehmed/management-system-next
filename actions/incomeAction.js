c94821472a60ae20459031160f248d4a6f25e632
ersin <ersin.hyusein@mypos.com>
2025-08-01 10:57:46 +0300

    Show all periods in dashboard boxes

--- actions/incomeAction.js
+++ actions/incomeAction.js
@@ -1,114 +1,114 @@
-import { getPeriodParam, fetchData } from "@/utils";
+import {getPeriodParam, fetchData} from "@/utils";
 
 class Income {
-  createIncome = async (data) => {
-    try {
-      const response = await fetch("/api/additional-incomes", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+    createIncome = async (data) => {
+        try {
+            const response = await fetch("/api/additional-incomes", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
 
-      return response.json();
-    } catch (error) {
-      throw error;
-    }
-  };
+            return response.json();
+        } catch (error) {
+            throw error;
+        }
+    };
 
-  getIncomes = async (period) => {
-    try {
-      const periodParam = getPeriodParam(period);
-      const userId = localStorage.getItem("userId");
-      const response = await fetch(
-        `/api/incomes?${periodParam}&userId=${userId}`
-      );
+    getIncomes = async (period) => {
+        try {
+            const periodParam = getPeriodParam(period);
+            const userId = localStorage.getItem("userId");
+            const response = await fetch(
+                `/api/incomes?${periodParam}&userId=${userId}`
+            );
 
-      return await response.json();
-    } catch (error) {
-      throw error;
-    }
-  };
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    };
 
-  getAdditionalIncomes = async (period) => {
-    try {
-      const periodParam = getPeriodParam(period);
-      const response = await fetch(`/api/additional-incomes?${periodParam}`);
+    getAdditionalIncomes = async (period) => {
+        try {
+            const periodParam = getPeriodParam(period);
+            const response = await fetch(`/api/additional-incomes?${periodParam}`);
 
-      return await response.json();
-    } catch (error) {
-      throw error;
-    }
-  };
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    };
 
-  getAverageProfit = async (period) => {
-    if (!period?.dateFrom && !period?.dateTo) {
-      const currentDate = new Date();
-      const startOfYear = new Date(currentDate.getFullYear(), 0, 1);
+    getAverageProfit = async (period) => {
+        if (!period?.dateFrom && !period?.dateTo) {
+            const currentDate = new Date();
+            const startOfYear = new Date(currentDate.getFullYear(), 0, 1);
 
-      period = {
-        dateFrom: "2023-12-31" /*startOfYear.toISOString().split("T")[0]*/,
-        dateTo: currentDate.toISOString().split("T")[0],
-      };
-    }
+            period = {
+                dateFrom: "2023-12-31" /*startOfYear.toISOString().split("T")[0]*/,
+                dateTo: currentDate.toISOString().split("T")[0],
+            };
+        }
 
-    try {
-      const response = await fetch(
-        `/api/get-average-profit?dateFrom=${period.dateFrom}&dateTo=${period.dateTo}`
-      );
+        try {
+            const response = await fetch(
+                `/api/get-average-profit?dateFrom=${period.dateFrom}&dateTo=${period.dateTo}`
+            );
 
-      return await response.json();
-    } catch (error) {
-      throw error;
-    }
-  };
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    };
 
-  getSaleIncomes = async (period) => {
-    try {
-      const periodParam = getPeriodParam(period);
-      const response = await fetch(
-        `/api/sales/get-total-incomes?${periodParam}`
-      );
+    getSaleIncomes = async (period) => {
+        try {
+            const periodParam = getPeriodParam(period);
+            const response = await fetch(
+                `/api/sales/get-total-incomes?${periodParam}`
+            );
 
-      return await response.json();
-    } catch (error) {
-      throw error;
-    }
-  };
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    };
 
-  getAllIncomes = async (
-    page,
-    perPage,
-    searchText,
-    filterData,
-    orderColumn
-  ) => {
-    return await fetchData(
-      "get-additional-incomes",
-      page,
-      perPage,
-      "",
-      "",
-      orderColumn
-    );
-  };
+    getAllIncomes = async (
+        page,
+        perPage,
+        searchText,
+        filterData,
+        orderColumn
+    ) => {
+        return await fetchData(
+            "get-additional-incomes",
+            page,
+            perPage,
+            "",
+            "",
+            orderColumn
+        );
+    };
 
-  IncomeSell = async (id) => {
-    try {
-      const response = await fetch(`/api/additional-incomes?id=${id}`, {
-        method: "DELETE",
-      });
+    IncomeSell = async (id) => {
+        try {
+            const response = await fetch(`/api/additional-incomes?id=${id}`, {
+                method: "DELETE",
+            });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-      return await response.json();
-    } catch (error) {
-      throw error;
-    }
-  };
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    };
 }
 
 export default new Income();
