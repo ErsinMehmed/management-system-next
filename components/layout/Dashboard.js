21b084c1046b14f21361cee4b4e36ceb8432c6a6
ersin <ersin.hyusein@mypos.com>
2025-08-06 11:50:36 +0300

    Improve products query load

--- components/layout/Dashboard.js
+++ components/layout/Dashboard.js
@@ -1,59 +1,59 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
+import React, {useState, useEffect} from "react";
+import {useSession} from "next-auth/react";
 import Navbar from "@/components/dashboard/Navbar";
 import SideBar from "@/components/dashboard/Sidebar";
 import MobileMenu from "@/components/dashboard/MobileMenu";
 import Alert from "@/components/Alert";
-import { productStore } from "@/stores/useStore";
+import {productStore} from "@/stores/useStore";
 
 const DashboardLayout = (props) => {
-  const { data: session } = useSession();
-  const { loadProducts } = productStore;
-  const [isVisible, setIsVisible] = useState(false);
+    const {data: session} = useSession();
+    const {loadProductsIfNotLoaded} = productStore;
+    const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    if (session?.user?.id && session?.user?.role) {
-      localStorage.setItem("userId", session.user.id);
-      localStorage.setItem("userRole", session.user.role);
-    }
+    useEffect(() => {
+        if (session?.user?.id && session?.user?.role) {
+            localStorage.setItem("userId", session.user.id);
+            localStorage.setItem("userRole", session.user.role);
+        }
 
-    loadProducts();
-  }, []);
+        loadProductsIfNotLoaded();
+    }, []);
 
-  const toggleMenu = () => {
-    setIsVisible(!isVisible);
-  };
+    const toggleMenu = () => {
+        setIsVisible(!isVisible);
+    };
 
-  return (
-    <>
-      <Alert />
+    return (
+        <>
+            <Alert/>
 
-      <div className='flex items-center w-full bg-[#f5f5f7]'>
-        <SideBar show={isVisible} />
+            <div className='flex items-center w-full bg-[#f5f5f7]'>
+                <SideBar show={isVisible}/>
 
-        <MobileMenu show={isVisible} />
+                <MobileMenu show={isVisible}/>
 
-        <div
-          className={`${
-            isVisible ? "sm:ml-16" : "sm:ml-52 2xl:ml-72"
-          } transition-all duration-500 w-full min-h-screen`}>
-          <Navbar
-            onMenuClick={toggleMenu}
-            show={isVisible}
-          />
+                <div
+                    className={`${
+                        isVisible ? "sm:ml-16" : "sm:ml-52 2xl:ml-72"
+                    } transition-all duration-500 w-full min-h-screen`}>
+                    <Navbar
+                        onMenuClick={toggleMenu}
+                        show={isVisible}
+                    />
 
-          <div className='mt-16'>
-            <div className='p-5 text-xl 2xl:text-2xl font-semibold text-slate-600 border-b border-gray-300'>
-              {props.title}
+                    <div className='mt-16'>
+                        <div className='p-5 text-xl 2xl:text-2xl font-semibold text-slate-600 border-b border-gray-300'>
+                            {props.title}
+                        </div>
+
+                        <div className='p-4 sm:p-5'>{props.children}</div>
+                    </div>
+                </div>
             </div>
-
-            <div className='p-4 sm:p-5'>{props.children}</div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default DashboardLayout;
--- stores/productStore.js
+++ stores/productStore.js
@@ -1,76 +1,82 @@
-import { makeObservable, observable, action } from "mobx";
+import {makeObservable, observable, action} from "mobx";
 import productAction from "@/actions/productAction";
-import { validateFields } from "@/utils";
-import { productRules as getProductRules } from "@/rules/product";
+import {validateFields} from "@/utils";
+import {productRules as getProductRules} from "@/rules/product";
 import commonStore from "./commonStore";
 
 class Product {
-  products = [];
-  productData = {};
-  isLoading = true;
-  isProductUpdated = false;
+    products = [];
+    productData = {};
+    isLoading = true;
+    isProductUpdated = false;
 
-  constructor() {
-    makeObservable(this, {
-      products: observable,
-      productData: observable,
-      isProductUpdated: observable,
-      isLoading: observable,
-      setProducts: action,
-      setProductData: action,
-    });
-  }
-
-  setProducts = (data) => {
-    this.products = data;
-  };
-
-  setProductData = (data) => {
-    this.productData = data;
-  };
-
-  loadProducts = async () => {
-    try {
-      const products = await productAction.getProducts();
-      this.setProducts(products);
-    } finally {
-      this.isLoading = false;
-    }
-  };
-
-  updateProduct = async (id, data) => {
-    commonStore.setErrorFields({});
-    commonStore.setErrorMessage(null);
-    commonStore.setSuccessMessage(null);
-    this.isProductUpdated = true;
-
-    data.price = parseFloat(data.price);
-    data.availability = parseFloat(data.availability);
-
-    const productRules = getProductRules();
-    const errorFields = validateFields(this.productData, productRules);
-
-    if (errorFields) {
-      commonStore.setErrorFields(errorFields);
-      this.isProductUpdated = false;
-
-      return false;
+    constructor() {
+        makeObservable(this, {
+            products: observable,
+            productData: observable,
+            isProductUpdated: observable,
+            isLoading: observable,
+            setProducts: action,
+            setProductData: action,
+        });
     }
 
-    const response = await productAction.updateProduct(id, data);
+    setProducts = (data) => {
+        this.products = data;
+    };
 
-    if (response.status) {
-      commonStore.setSuccessMessage(response.message);
-      this.loadProducts();
-      this.isProductUpdated = false;
+    setProductData = (data) => {
+        this.productData = data;
+    };
 
-      return true;
-    }
+    loadProducts = async () => {
+        try {
+            const products = await productAction.getProducts();
+            this.setProducts(products);
+        } finally {
+            this.isLoading = false;
+        }
+    };
 
-    this.isProductUpdated = false;
+    loadProductsIfNotLoaded = async () => {
+        if (this.products.length === 0) {
+            await this.loadProducts();
+        }
+    };
 
-    return false;
-  };
+    updateProduct = async (id, data) => {
+        commonStore.setErrorFields({});
+        commonStore.setErrorMessage(null);
+        commonStore.setSuccessMessage(null);
+        this.isProductUpdated = true;
+
+        data.price = parseFloat(data.price);
+        data.availability = parseFloat(data.availability);
+
+        const productRules = getProductRules();
+        const errorFields = validateFields(this.productData, productRules);
+
+        if (errorFields) {
+            commonStore.setErrorFields(errorFields);
+            this.isProductUpdated = false;
+
+            return false;
+        }
+
+        const response = await productAction.updateProduct(id, data);
+
+        if (response.status) {
+            commonStore.setSuccessMessage(response.message);
+            this.loadProducts();
+            this.isProductUpdated = false;
+
+            return true;
+        }
+
+        this.isProductUpdated = false;
+
+        return false;
+    };
 }
 
 export default new Product();
